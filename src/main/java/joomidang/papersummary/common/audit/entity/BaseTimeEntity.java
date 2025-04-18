129591877f410addbc53d5b5e5cae649754b735a
Byuntil <gugugu2014@jbnu.ac.kr>
2025-04-18 19:53:04 +0900

    chore: BaseTimeEntity JPA 생명주기 이벤트 방식으로 변경

--- src/main/java/joomidang/papersummary/common/audit/entity/BaseTimeEntity.java
+++ src/main/java/joomidang/papersummary/common/audit/entity/BaseTimeEntity.java
@@ -1,27 +1,32 @@
 package joomidang.papersummary.common.audit.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(value = {AuditingEntityListener.class})
 public class BaseTimeEntity {
-    @CreatedDate
-    @DateTimeFormat(iso = ISO.DATE_TIME)
-    @Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
+    @Column(nullable = false, updatable = false)
     protected LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @DateTimeFormat(iso = ISO.DATE_TIME)
-    @Column(columnDefinition = "TIMESTAMP")
+    @Column
     protected LocalDateTime updatedAt;
+
+    @Column(columnDefinition = "TINYINT(1) DEFAULT 0", nullable = false)
+    protected boolean isDeleted = false;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = null;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
