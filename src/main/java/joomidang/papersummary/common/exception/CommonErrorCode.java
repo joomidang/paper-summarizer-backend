734491fd116a187a3004992c78980e9c26328c32
Byuntil <gugugu2014@jbnu.ac.kr>
2025-04-20 23:47:42 +0900

    refactor: 에러 코드 표기 변경

--- src/main/java/joomidang/papersummary/common/exception/CommonErrorCode.java
+++ src/main/java/joomidang/papersummary/common/exception/CommonErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE) //생성자 접근 제한
 @Getter
 public enum CommonErrorCode implements ErrorCode {
-    INTERNAL_SERVER_ERROR("500"),
+    INTERNAL_SERVER_ERROR("IA-0001"),
     ;
     private final String value;
 }
