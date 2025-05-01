a097dd0af79f2272f5d21c201428ecc005a07f16
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-01 23:19:32 +0900

    feat: paper관련 커스텀 예외 추가

--- src/main/java/joomidang/papersummary/paper/exception/InvalidFileTypeException.java
+++ src/main/java/joomidang/papersummary/paper/exception/InvalidFileTypeException.java
@@ -1,7 +1,9 @@
 package joomidang.papersummary.paper.exception;
 
 import joomidang.papersummary.paper.controller.response.PaperErrorCode;
+import lombok.Getter;
 
+@Getter
 public class InvalidFileTypeException extends RuntimeException {
     private final PaperErrorCode errorCode;
 
@@ -14,7 +16,4 @@ public class InvalidFileTypeException extends RuntimeException {
         this("지원하지 않는 파일 형식입니다. PDF 파일만 업로드 가능합니다.");
     }
 
-    public PaperErrorCode getErrorCode() {
-        return errorCode;
-    }
 }
