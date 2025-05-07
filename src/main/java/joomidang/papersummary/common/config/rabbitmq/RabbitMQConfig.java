1b7c1eb8ceb712ce207e67a018c90d62c612cd0c
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-08 00:21:28 +0900

    feat: RabbitMQ 재시도 방지 로직 수정

--- src/main/java/joomidang/papersummary/common/config/rabbitmq/RabbitMQConfig.java
+++ src/main/java/joomidang/papersummary/common/config/rabbitmq/RabbitMQConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -79,5 +80,14 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(completeQueue())
                 .to(paperExchange())
                 .with(ROUTING_KEY_COMPLETE);
+    }
+
+    @Bean(name = "rabbitListenerContainerFactory")
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        factory.setDefaultRequeueRejected(false); // 예외 발생 시 메시지를 재큐하지 않음
+        return factory;
     }
 }
