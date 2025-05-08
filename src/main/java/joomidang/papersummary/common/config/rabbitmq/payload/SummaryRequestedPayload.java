94887dba559112837151dd17859dc995476c6e61
zzl-hyun <kimgihyun877@gmail.com>
2025-05-09 02:21:54 +0900

    fix: payload에 contentListUrl 추가

--- src/main/java/joomidang/papersummary/common/config/rabbitmq/payload/SummaryRequestedPayload.java
+++ src/main/java/joomidang/papersummary/common/config/rabbitmq/payload/SummaryRequestedPayload.java
@@ -3,6 +3,7 @@ package joomidang.papersummary.common.config.rabbitmq.payload;
 public record SummaryRequestedPayload(
         Long paperId,
         String markdownUrl,
+        String contentListUrl,
         String prompt,
         String language
 ) {
