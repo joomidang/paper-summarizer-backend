f697e38cad647466e28c1d4cf9a516ced5de12cc
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-09 00:50:28 +0900

    fix: 쿠키로 토큰 인증 수정

--- src/main/java/joomidang/papersummary/auth/resolver/AuthenticatedArgumentResolver.java
+++ src/main/java/joomidang/papersummary/auth/resolver/AuthenticatedArgumentResolver.java
@@ -6,10 +6,12 @@ import java.util.Arrays;
 import java.util.Optional;
 import joomidang.papersummary.auth.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -17,6 +19,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthenticatedArgumentResolver implements HandlerMethodArgumentResolver {
@@ -32,16 +35,14 @@ public class AuthenticatedArgumentResolver implements HandlerMethodArgumentResol
     public Object resolveArgument(MethodParameter parameter,
                                   ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory) throws Exception {
+                                  WebDataBinderFactory binderFactory) {
 
-        // 2. 헤더에서 토큰 확인
-        final String authorizationHeader = webRequest.getHeader("Authorization");
-        if (StringUtils.hasText(authorizationHeader)) {
-            return tokenProvider.getUserId(authorizationHeader);
-        }
         // 1. 쿠키에서 토큰 확인
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        log.debug("헤더 Authorization: {}", webRequest.getHeader("Authorization"));
+
         if (request != null && request.getCookies() != null) {
+            log.debug("쿠키 정보: {}", Arrays.toString(request.getCookies()));
             Optional<Cookie> tokenCookie = Arrays.stream(request.getCookies())
                     .filter(c -> "accessToken".equals(c.getName()))
                     .findFirst();
@@ -50,13 +51,22 @@ public class AuthenticatedArgumentResolver implements HandlerMethodArgumentResol
                 String token = "Bearer " + tokenCookie.get().getValue(); // Bearer 접두사 추가
                 return tokenProvider.getUserId(token);
             }
+        } else {
+            log.debug("요청에 쿠키가 없음");
+        }
+        // 2. 헤더에서 토큰 확인
+        final String authorizationHeader = webRequest.getHeader("Authorization");
+        if (StringUtils.hasText(authorizationHeader)) {
+            return tokenProvider.getUserId(authorizationHeader);
         }
 
         // 3. SecurityContext에서 확인
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated() &&
-                !(authentication.getPrincipal() instanceof String)) {
-            return authentication.getName();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof User user) {
+                return user.getUsername();
+            }
         }
 
         throw new BadCredentialsException("인증 정보를 찾을 수 없습니다");
