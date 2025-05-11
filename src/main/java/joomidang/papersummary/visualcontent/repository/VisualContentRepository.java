10cd2dcb8811902f24cbf64e2c154d82b1501f6a
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-11 21:46:20 +0900

    fix: VisualContent에 summary 연결 로직 오류 수정

--- src/main/java/joomidang/papersummary/visualcontent/repository/VisualContentRepository.java
+++ src/main/java/joomidang/papersummary/visualcontent/repository/VisualContentRepository.java
@@ -1,17 +1,20 @@
 package joomidang.papersummary.visualcontent.repository;
 
 import java.util.List;
+import java.util.Optional;
 import joomidang.papersummary.summary.entity.Summary;
 import joomidang.papersummary.visualcontent.entity.VisualContent;
 import joomidang.papersummary.visualcontent.entity.VisualContentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VisualContentRepository extends JpaRepository<VisualContent, Long> {
-    List<VisualContent> paperId(Long paperId);
-
-    List<VisualContent> findByPaperIdAndSummaryIsNull(Long summaryId);
+    List<VisualContent> findByPaperIdAndSummaryIsNull(Long paperId);
 
     List<VisualContent> findBySummaryAndType(Summary summary, VisualContentType type);
+
+    @Query("SELECT MAX(v.position) FROM VisualContent v WHERE v.summary.id = :summaryId")
+    Optional<Integer> findMaxPositionBySummaryId(Long summaryId);
 }
