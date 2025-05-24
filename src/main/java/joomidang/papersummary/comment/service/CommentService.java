8dd858c356a51b67b3926f7e4f3949a2a53e78d1
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-24 17:21:21 +0900

    feat: 댓글 수정 기능 구현

--- src/main/java/joomidang/papersummary/comment/service/CommentService.java
+++ src/main/java/joomidang/papersummary/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package joomidang.papersummary.comment.service;
 
 import joomidang.papersummary.comment.controller.response.CommentResponse;
 import joomidang.papersummary.comment.entity.Comment;
+import joomidang.papersummary.comment.exception.CommentAccessDeniedException;
 import joomidang.papersummary.comment.exception.CommentNotFoundException;
 import joomidang.papersummary.comment.exception.InvalidParentCommentException;
 import joomidang.papersummary.comment.repository.CommentRepository;
@@ -13,9 +14,11 @@ import joomidang.papersummary.summary.service.SummaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
@@ -25,6 +28,7 @@ public class CommentService {
     /**
      * 댓글 작성
      */
+    @Transactional
     public CommentResponse createComment(String providerUid, Long summaryId, String content) {
         log.debug("댓글 작성 시작: summaryId={}, providerUid={}", summaryId, providerUid);
         Summary summary = summaryService.findByIdWithoutStats(summaryId);
@@ -42,6 +46,7 @@ public class CommentService {
     /**
      * 대댓글 작성
      */
+    @Transactional
     public CommentResponse createReply(String providerUid, Long summeryId, Long parentCommentId, String content) {
         log.debug("대댓글 작성 시작: parentCommentId");
         Summary summary = validateSummaryForComment(summeryId);
@@ -63,6 +68,23 @@ public class CommentService {
         log.debug("대댓글 작성 완료 : replyCommentId={}", savedReplyComment.getId());
 
         return CommentResponse.from(savedReplyComment);
+    }
+
+    /**
+     * 댓글 수정
+     */
+    @Transactional
+    public CommentResponse updateComment(String providerUid, Long commentId, String content) {
+        log.debug("댓글 수정 시작: commentId={}, providerUid={}", commentId, providerUid);
+        Comment comment = findCommentById(commentId);
+
+        validateCommentNotDeleted(comment);
+
+        validateCommentOwnership(comment, providerUid);
+
+        comment.updateContent(content);
+
+        return CommentResponse.from(comment);
     }
 
     /**
@@ -100,6 +122,27 @@ public class CommentService {
             log.error("부모 댓글과 요약본 불일치: parentCommentId={}, summeryId={}", parentComment.getSummary().getId(),
                     summeryId);
             throw new InvalidParentCommentException(parentCommentId);
+        }
+    }
+
+    /**
+     * 댓글이 삭제되지 않았는지 확인
+     */
+    private void validateCommentNotDeleted(Comment comment) {
+        if (comment.isDeleted()) {
+            log.error("삭제된 댓글에 대한 작업 시도: commentId={}", comment.getId());
+            throw new IllegalArgumentException("삭제된 댓글입니다.");
+        }
+    }
+
+    /**
+     * 댓글 소유권 확인 (작성자인지 확인)
+     */
+    private void validateCommentOwnership(Comment comment, String providerUid) {
+        if (!comment.getMember().getProviderUid().equals(providerUid)) {
+            log.error("댓글 소유권 없음: commentId={}, 요청자={}, 작성자={}",
+                    comment.getId(), providerUid, comment.getMember().getProviderUid());
+            throw new CommentAccessDeniedException();
         }
     }
 
