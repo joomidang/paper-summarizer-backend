489c4e03a9217bf76b219cee2aef6069421e7236
zzl-hyun <kimgihyun877@gmail.com>
2025-05-27 16:36:41 +0900

    feature: 사용자 좋아요한 요약 목록 조회 API

--- src/main/java/joomidang/papersummary/member/service/MemberService.java
+++ src/main/java/joomidang/papersummary/member/service/MemberService.java
@@ -303,15 +303,4 @@ public class MemberService {
     }
 
 
-//    @Transactional
-//    public MemberSummaryResponse getLikedSummaries(final Long memberId, int page, int size){
-//        log.debug("좋아요한 요약 목록 조회: providerUid={}, page={}, size={}", providerUid, page, size);
-//        if (page > 0) page = page - 1;
-//
-//        // 기본 정렬: 생성일 기준 내림차순 (최신순)
-//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
-//        Page<Summary> likedSummaryPage = summaryRepository.findSummariesByMemberIdWithlikes(memberId, pageable);
-//        return MemberSummaryResponse.from(likedSummaryPage);
-//    }
-
 }
