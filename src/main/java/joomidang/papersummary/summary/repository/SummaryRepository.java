281d33cf11e94e770e342483601c8d8cbf9aecdf
Byuntil <gugugu2014@jbnu.ac.kr>
2025-05-27 21:50:44 +0900

    feat: 인기 요약본 목록 조회 기능 구현

--- src/main/java/joomidang/papersummary/summary/repository/SummaryRepository.java
+++ src/main/java/joomidang/papersummary/summary/repository/SummaryRepository.java
@@ -1,7 +1,9 @@
 package joomidang.papersummary.summary.repository;
 
+import java.util.List;
 import java.util.Optional;
 import joomidang.papersummary.paper.entity.Paper;
+import joomidang.papersummary.summary.entity.PublishStatus;
 import joomidang.papersummary.summary.entity.Summary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,8 +23,26 @@ public interface SummaryRepository extends JpaRepository<Summary, Long> {
     boolean existsByPaper(Paper paper);
 
     @Query(value = "SELECT s FROM Summary s WHERE s.member.id = :memberId AND s.isDeleted = false",
-           countQuery = "SELECT COUNT(s) FROM Summary s WHERE s.member.id = :memberId AND s.isDeleted = false")
+            countQuery = "SELECT COUNT(s) FROM Summary s WHERE s.member.id = :memberId AND s.isDeleted = false")
     Page<Summary> findByMemberIdWithStats(@Param("memberId") Long memberId, Pageable pageable);
 
-//    Page<Summary> findSummariesByMemberIdWithlikes
+
+    @Query(value = "SELECT s FROM Summary s JOIN FETCH s.summaryStats st JOIN FETCH s.member m "
+            + "WHERE s.publishStatus = :publishStatus AND s.isDeleted = false "
+            + "ORDER BY ("
+            + "COALESCE(st.likeCount,0)*0.5 +"
+            + "COALESCE(st.commentCount,0)*0.3 +"
+            + "COALESCE(st.viewCount, 0)*0.2"
+            + ") DESC, s.updatedAt DESC",
+            countQuery = "SELECT COUNT(s) FROM Summary s WHERE s.publishStatus = :publishStatus AND s.isDeleted = false")
+    Page<Summary> findPopularSummariesByPublishStatus(@Param("publishStatus") PublishStatus publishStatus,
+                                                      Pageable pageable);
+
+    @Query("SELECT s.id, (COALESCE(st.likeCount, 0)*0.5 +"
+            + "COALESCE(st.commentCount, 0)*0.3 + "
+            + "COALESCE(st.viewCount, 0)*0.2) "
+            + "FROM Summary s "
+            + "LEFT JOIN s.summaryStats st "
+            + "WHERE s.id IN :summaryIds")
+    List<Object[]> calculatePopularityScores(@Param("summaryIds") List<Long> summaryIds);
 }
